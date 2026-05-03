16e536ddbb3d5ac234d0f63c4cc099c30feabf6f
abraar a <70454386+abraaralam@users.noreply.github.com>
2026-05-03 08:28:25 -0400

    Refactor ImmGen to use ImmSrc for immediate selection

--- src/imm_gen.sv
+++ src/imm_gen.sv
@@ -1,38 +1,35 @@
 `timescale 1ns / 1ps
 
 module ImmGen(
-input logic [31:0] instr,
-output logic [31:0] immExt
-    );
-    logic [6:0] opcode;
-    assign opcode = instr[6:0];
+    input logic [31:0] instr,
+    input logic [1:0] ImmSrc, // selects immediate type
+    output logic [31:0] immExt
+);
+
     always_comb begin
     
-    case (opcode) 
+    case (ImmSrc) 
     
-    // according to this diagram: https://docs.riscv.org/reference/isa/unpriv/rv32.html#immtypes 
-    // and this ref sheet: https://www.cs.sfu.ca/~ashriram/Courses/CS295/assets/notebooks/RISCV/RISCV_CARD.pdf
+    // according to Table 7.5 of Harris and Harris: Digital Design and Computer Architecture RISCV Edition  
+    
         
         // I instruction
         // leftmost immediate digit extended to be 32 bits wide 
-        
-        7'b0010011: immExt = {{21{instr[31]}}, instr[30:20]}; 
+        2'b00: immExt = {{20{instr[31]}}, instr[31:20]};  
         
         // S instruction
-        7'b0100011: immExt = {{21{instr[31]}}, instr[30:25], instr[11:7]};
+        2'b01: immExt = {{21{instr[31]}}, instr[30:25], instr[11:7]};
         
         // B instruction
-        7'b1100011: immExt = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
-        
-        // U instruction, both 0110111 and 0010111
-        7'b0110111, 
-        7'b0010111: immExt = {instr[31:12], 12'b0};
+        2'b10: immExt = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
         
         // J instruction
-        7'b1101111: immExt = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
+        2'b11: immExt = {instr[31], instr[19:12], instr[20], instr[30:21], 1'b0}; 
         
         // default 
         default: immExt = 32'b0;
+        
     endcase
     end
+
 endmodule
